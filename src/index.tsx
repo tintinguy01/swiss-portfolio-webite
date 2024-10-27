21c9b084c16f0d7716a6770100370354ffe3a668
Swiss Tangsatjatham <tintinguy.01@gmail.com>
2024-10-27 14:16:09 +0700

    Updated responsiveness

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/swiss-portfolio-webite">
       <div id="stars"></div>
       <div id="stars2"></div>
       <div id="stars3"></div>
