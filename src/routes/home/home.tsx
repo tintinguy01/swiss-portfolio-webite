8d7ee022a8e868ad7eda389d1d1e33fffb464f56
Swiss Tangsatjatham <tintinguy01@gmail.com>
2024-10-28 16:44:36 +0700

    Updated resume file

--- src/routes/home/home.tsx
+++ src/routes/home/home.tsx
@@ -128,12 +128,13 @@ const Home = () => {
           className="resume-button"
           onClick={() => {
             const link = document.createElement('a');
-            link.href = "/files/Swiss-Resume.pdf";
-            link.download = "swiss-resume.pdf";
+            link.href = `${process.env.PUBLIC_URL}/Swiss-Resume.pdf`;
+            link.download = "Swiss-Resume.pdf";
             document.body.appendChild(link);
             link.click();
             document.body.removeChild(link);
-          }}>
+          }}
+          >
           <FileText size={20} />
         </IconButton>
       </div>
